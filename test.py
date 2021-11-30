6620932fa0140b8179c76ce932e919c49685f061
JS Kim <wotjr4836@naver.com>
2021-11-30 18:34:52 +0900

    no message

---
+++ test.py
@@ -0,0 +1,8 @@
+import pyupbit
+
+access = "8fdKhUQdikizHtYeJgw9mo1QvJx8c5lhzpq0KnRq"          # 본인 값으로 변경
+secret = "2KC2Q8sqV7NLD0JD8uhATHjdJXsJOz6A1aBXMcxl"          # 본인 값으로 변경
+upbit = pyupbit.Upbit(access, secret)
+
+print(upbit.get_balance("KRW-XRP"))     # KRW-XRP 조회
+print(upbit.get_balance("KRW"))         # 보유 현금 조회
